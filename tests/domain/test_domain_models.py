9850ff3c8530cbfd11ac8d09d849ccac4ad5652d
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2024-01-27 02:23:46 +0100

    Add test if chat_factory returns chat

--- tests/domain/test_domain_models.py
+++ tests/domain/test_domain_models.py
@@ -8,5 +8,12 @@ from chatcleaner.domain.model.schemas import ChatCreateDTO
 def test_if_chat_is_created():
     schema_ = ChatCreateDTO()
     result = schema_.load({"chat": "test"})
-    model = Chat(**result)
-    assert model.chat == "test"
+    model_ = Chat(**result)
+    assert model_.chat == "test"
+
+
+def test_if_chat_is_created_with_factory():
+    schema_ = ChatCreateDTO()
+    result = schema_.load({"chat": "test"})
+    model_ = chat_factory(**result)
+    assert model_.chat == "test"
