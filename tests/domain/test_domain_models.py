0412eb7390379453d71c063431b8e53e36624dff
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2024-01-27 02:31:31 +0100

    Add test for chat DTO

--- tests/domain/test_domain_models.py
+++ tests/domain/test_domain_models.py
@@ -17,3 +17,33 @@ def test_if_chat_is_created_with_factory():
     result = schema_.load({"chat": "test"})
     model_ = chat_factory(**result)
     assert model_.chat == "test"
+
+
+def test_if_chat_is_created_with_factory_and_wrong_type():
+    schema_ = ChatCreateDTO()
+    with pytest.raises(marshmallow.exceptions.ValidationError):
+        result = schema_.load({"chat": 1})
+        model_ = chat_factory(**result)
+        assert model_.chat == "test"
+
+
+def test_if_chat_create_dto_can_be_created_with_wrong_type():
+    with pytest.raises(marshmallow.exceptions.ValidationError):
+        schema_ = ChatCreateDTO()
+        result = schema_.load({"chat": 1})
+        assert result == {"chat": 1}
+
+
+def test_if_chat_dto_can_be_created_with_extra_fields():
+    schema_ = ChatCreateDTO()
+    result = schema_.load({"chat": "test", "extra": "test"})
+    # Extra fields should be excluded.
+    assert result == {"chat": "test"}
+
+
+def test_if_chat_dto_can_be_created_with_wrong_field_name():
+    with pytest.raises(marshmallow.exceptions.ValidationError):
+        schema_ = ChatCreateDTO()
+        result = schema_.load({"talk": "test"})
+        # Extra fields should be excluded.
+        assert result == {"chat": "test"}
