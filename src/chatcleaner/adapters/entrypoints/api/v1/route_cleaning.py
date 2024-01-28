78fa6d02b5938f282374edf1125749d65386c2ac
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2024-01-29 00:09:21 +0100

    feat: add get_by_uuid endpoint

--- src/chatcleaner/adapters/entrypoints/api/v1/route_cleaning.py
+++ src/chatcleaner/adapters/entrypoints/api/v1/route_cleaning.py
@@ -1,4 +1,5 @@
 import json
+from typing import Any
 
 from dependency_injector.wiring import Provide, inject
 from fastapi import APIRouter, Depends, Response
@@ -8,7 +9,7 @@ from chatcleaner.domain.ports.use_cases.clean import CleanUseCaseInterface
 router = APIRouter()
 
 
-@router.post("/clean", response_model=None)
+@router.post("/cleaning", response_model=None)
 @inject
 async def get_all_cleaning(
     use_case: CleanUseCaseInterface = Depends(Provide["clean_use_case"]),
@@ -17,3 +18,12 @@ async def get_all_cleaning(
     return Response(
         content=json.dumps(data), media_type="application/json", status_code=200
     )
+
+
+@router.post("/cleaning/{uuid}", response_model=None)
+@inject
+async def get_cleaning_by_uuid(
+    uuid: str,
+    use_case: CleanUseCaseInterface = Depends(Provide["clean_use_case"]),
+) -> dict[str, Any]:
+    return use_case.get_by_uuid(uuid)
