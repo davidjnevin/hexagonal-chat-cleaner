afc231346a0e96dafa5d375f8b6b865bda078572
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2024-01-28 22:10:34 +0100

    fix: rename use case add to use case clean

--- tests/integrations/test_cleaning_use_case.py
+++ tests/integrations/test_cleaning_use_case.py
@@ -1,10 +1,10 @@
 from chatcleaner.configurator.containers import Container
 
 
-def test_clean_use_case_add(get_fake_container, get_clean_use_case):
+def test_clean_use_case_clean(get_fake_container, get_clean_use_case):
     with Container.cleaning_uow.override(get_fake_container.cleaning_uow):
         with Container.chat_service.override(get_fake_container.chat_service):
-            get_clean_use_case.add("\n19:10:00 from David to Everyone:\ntest")
+            get_clean_use_case.clean("\n19:10:00 from David to Everyone:\ntest")
             uow_ = get_fake_container.cleaning_uow()
             with uow_:
                 result = uow_.cleaning.get_all()
