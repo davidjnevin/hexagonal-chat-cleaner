320a78b614114b98f2a0f923323c5546743c38a4
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2024-01-28 23:32:39 +0100

    feat: add get_by_uuid case use functionality and tests
    Changed get_all to produce dict of results only containing uuids

--- src/chatcleaner/domain/ports/use_cases/clean.py
+++ src/chatcleaner/domain/ports/use_cases/clean.py
@@ -1,4 +1,5 @@
 import abc
+from typing import Any
 
 from chatcleaner.domain.ports.services.chat import ChatServiceInterface
 from chatcleaner.domain.ports.unit_of_works.cleaning import CleaningUnitOfWorkInterface
@@ -15,10 +16,17 @@ class CleanUseCaseInterface(abc.ABC):
     def get_all(self):
         return self._get_all()
 
+    def get_by_uuid(self, uuid_: str) -> dict[str, Any]:
+        return self._get_by_uuid(uuid_)
+
     @abc.abstractmethod
     def _clean(self, chat: str) -> None:
         raise NotImplementedError
 
     @abc.abstractmethod
     def _get_all(self):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def _get_by_uuid(self, uuid_: str) -> dict[str, Any]:
         raise NotImplementedError
--- tests/integrations/test_cleaning_use_case.py
+++ tests/integrations/test_cleaning_use_case.py
@@ -23,12 +23,25 @@ def test_clean_use_case_get_all(get_fake_container, get_clean_use_case):
             with uow_:
                 result = get_clean_use_case.get_all()
                 # fixtures are scopes to module, so this should be 4
-                assert len(result) == 4
-                assert result[0].chat == "\n19:10:00 from David to Everyone:\ntest"
-                assert result[0].cleaned_chat == "test"
-                assert result[1].chat == "\n19:10:00 from David to Everyone:\ntest 1"
-                assert result[1].cleaned_chat == "test 1"
-                assert result[2].chat == "\n19:10:00 from David to Everyone:\ntest 2"
-                assert result[2].cleaned_chat == "test 2"
-                assert result[3].chat == "\n19:10:00 from David to Everyone:\ntest 3"
-                assert result[3].cleaned_chat == "test 3"
+                assert len(result["results"]) == 4
+                # breakpoint()
+                # assert result[0].chat == "\n19:10:00 from David to Everyone:\ntest"
+                # assert result[0].cleaned_chat == "test"
+                # assert result[1].chat == "\n19:10:00 from David to Everyone:\ntest 1"
+                # assert result[1].cleaned_chat == "test 1"
+                # assert result[2].chat == "\n19:10:00 from David to Everyone:\ntest 2"
+                # assert result[2].cleaned_chat == "test 2"
+                # assert result[3].chat == "\n19:10:00 from David to Everyone:\ntest 3"
+                # assert result[3].cleaned_chat == "test 3"
+
+
+def test_clean_use_case_get_by_id(get_fake_container, get_clean_use_case):
+    with Container.cleaning_uow.override(get_fake_container.cleaning_uow):
+        with Container.chat_service.override(get_fake_container.chat_service):
+            result = get_clean_use_case.get_all()
+            uuid_ = result["results"][0]
+            assert uuid_
+            data = get_clean_use_case.get_by_uuid(uuid_)
+            assert data["result"]["uuid"] == uuid_
+            data = get_clean_use_case.get_by_uuid("fake")
+            assert data["result"] == "Not found"
