74e813b4c1192ab0586bf3e6236ea57d29591bed
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2024-01-29 12:53:09 +0100

    feat: add CORS middleware
    useful for testing and future access limting

--- src/chatcleaner/adapters/entrypoints/api/app.py
+++ src/chatcleaner/adapters/entrypoints/api/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from chatcleaner.adapters.db.orm import start_mappers
 from chatcleaner.adapters.entrypoints.api.base import api_router
@@ -9,11 +10,29 @@ def include_router(app_):
     app_.include_router(api_router)
 
 
+def configure_cors(app_):
+    origins = [
+        "*",  # Allow all
+    ]
+    # Allow these methods to be used
+    methods = ["GET", "POST", "PUT", "DELETE"]
+    headers = ["*"]
+
+    app_.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=methods,
+        allow_headers=headers,
+    )
+
+
 def start_application():
     container = Container()
     app_ = FastAPI(title="Chat Cleaner")
     app_.container = container
     include_router(app_)
+    configure_cors(app_)
     # start orm mappers
     try:
         start_mappers()
