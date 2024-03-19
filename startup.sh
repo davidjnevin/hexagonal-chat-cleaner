8f2a0b3bf4b9062f58deb5cd07a87e50138f682f
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2024-03-20 00:00:26 +0100

    add versions folder check to enable migrations

--- startup.sh
+++ startup.sh
@@ -1,3 +1,18 @@
+#!/bin/bash
+
+# Define the directory path relative to the script
+target_dir="/src/chatcleaner/adapters/db/migrations/versions"
+
+# Check if the directory exists
+if [ ! -d "$target_dir" ]; then
+  # Create the directory if it doesn't exist
+  echo "Creating directory: $target_dir"
+  mkdir -p "$target_dir"
+fi
+
+echo "Versions directory check complete."
+
+
 # Conditional creation of .env file
 if [[ -f ".env" ]]; then
   echo "env variables file found"
@@ -7,3 +22,5 @@ else
 fi
 
 echo "environmnet variables set"
+
+echo "Startup script complete."
