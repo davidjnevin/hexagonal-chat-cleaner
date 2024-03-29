55e9e1b360726d00d160f69696625baf6c7264a1
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2024-03-29 20:44:52 +0100

    add flake8, pytype and black formatting

--- src/chatcleaner/__init__.py
+++ src/chatcleaner/__init__.py
@@ -1,3 +1,3 @@
-"""chat-cleaner - A web app to clean the Zoom chat generated *.txt files of speakers names and datetime details."""
+"""chat-cleaner - A web app to clean the Zoom chat generated *.txt files of speakers names and datetime details."""  # noqa: E501
 
 __version__ = "0.0.1"
