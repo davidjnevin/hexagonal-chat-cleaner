09b14e9e881a7f08cc0556603fa54d67ca83ed3e
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2024-04-04 13:29:51 +0200

    change forwarded-allow-ips to use '='

--- entrypoint.sh
+++ entrypoint.sh
@@ -59,7 +59,7 @@ case $1 in
             --reload \
 			--host 0.0.0.0 \
 			--proxy-headers \
-			--forwarded-allow-ips * \
+			--forwarded-allow-ips="*" \
 			--port 8088
 		else
 			uvicorn \
@@ -67,7 +67,7 @@ case $1 in
 				--workers 2 \
 				--host 0.0.0.0 \
 				--proxy-headers \
-			    --forwarded-allow-ips * \
+			    --forwarded-allow-ips="*" \
 				--port 8088
 		fi
 	;;
